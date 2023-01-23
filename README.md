235c9b105f018bae82f0d53c6b39da67b6929b64
Veron <example@mail.com>
2023-01-24 00:16:39 +0300

    Counter commit

---
+++ README.md
@@ -0,0 +1,1 @@
+Simple counter using React + Redux
---
+++ src/index.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import {createStore} from 'redux';
+import { Provider } from 'react-redux';
+import reducer from './reducer';
+import App from './components/App';
+
+const store = createStore(reducer);
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+  <React.StrictMode>
+    <Provider store={store}>
+      <App/>
+    </Provider>
+  </React.StrictMode>
+);
